30e606bf63435d00e672ab66466a9461aacc0e43
MutheuMC <95241952+MutheuMC@users.noreply.github.com>
2024-03-04 10:12:07 +0300

    changed cors rule

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
    credentials: true,
-      origin: "https://eventgo-flame.vercel.app/",
+      origin: "https://eventgo-flame.vercel.app",
 }));
 
 mongoose.connect(process.env.MONGO_URL);
